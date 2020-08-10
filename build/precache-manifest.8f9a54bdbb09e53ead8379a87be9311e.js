50806fa2d667667affc35153b2deee7ff6bbb30d
alejandra66 <alejandra.ramos92@gmail.com>
2020-08-10 18:01:36 -0400

    first commit

---
+++ build/precache-manifest.8f9a54bdbb09e53ead8379a87be9311e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f794f3342015878a988c3e1a0cee37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e2dba0dcf3e4c19185",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "211dc71e1ff9e21b8777",
+    "url": "/static/js/2.9ae75c2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ae75c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e2dba0dcf3e4c19185",
+    "url": "/static/js/main.0d50a5b2.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
